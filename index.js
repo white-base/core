8d98521f04a99792bc87b4c400f9af7221f34795
logic <logicfeel@nate.com>
2023-03-28 14:33:42 +0900

    index 갱신, test 도 추가

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-// ES5
-//----------
-var BaseCollection = require("./src/collection-base");
-var ArrayCollection = require("./src/collection-array");
-var PropertyCollection = require("./src/collection-property");
-var MetaObject = require("./src/meta-object");
-var MetaElement = require("./src/meta-element");
-var Observer = require("./src/observer");
+/**
+ * ES5
+ */
+
+var Observer                    = require("./src/observer");
+var BaseCollection              = require("./src/collection-base");
+var ArrayCollection             = require("./src/collection-array");
+var PropertyCollection          = require("./src/collection-property");
+var PropertyFunctionCollection  = require('./src/collection-property-function');
+var MetaObject                  = require("./src/meta-object");
+var MetaElement                 = require("./src/meta-element");
+var Entity                      = require('./src/entity-base');
+var EntityView                  = require('./src/entity-view').EntityView;
+var EntityViewCollection        = require('./src/entity-view').EntityViewCollection;
+var EntityTable                 = require('./src/entity-table').EntityTable;
+var Item                        = require('./src/entity-item').Item;
+var CustomError                 = require('./src/error-custom');
+var utils                       = require('./src/utils');
 
 module.exports = {
     BaseCollection: BaseCollection,
@@ -14,6 +24,12 @@ module.exports = {
     MetaObject: MetaObject,
     MetaElement: MetaElement,
     Observer: Observer,
+    EntityView: EntityView,
+    EntityViewCollection: EntityViewCollection,
+    EntityTable: EntityTable,
+    Item: Item,
+    CustomError: CustomError,
+    Util: utils,
     Common: {
         Interface: null,
         Collection: null,
---
+++ test/index.test.js
@@ -0,0 +1,29 @@
+/**
+ * ES6 + CJS
+ */
+//==============================================================
+// gobal defined
+const index   = require('../');
+
+//==============================================================
+// test
+describe('index 타입 검사', () => {
+    beforeAll(() => {
+    });
+    it('- named export check ', () => {
+        expect(typeof index.BaseCollection).toBe('function');
+        expect(typeof index.ArrayCollection).toBe('function');
+        expect(typeof index.PropertyCollection).toBe('function');
+        expect(typeof index.MetaObject).toBe('function');
+        expect(typeof index.MetaElement).toBe('function');
+        expect(typeof index.Observer).toBe('function');
+        expect(typeof index.EntityView).toBe('function');
+        expect(typeof index.EntityViewCollection).toBe('function');
+        expect(typeof index.EntityTable).toBe('function');
+        expect(typeof index.Item).toBe('function');
+        expect(typeof index.CustomError).toBe('function');
+        expect(typeof index.Util.inherits).toBe('function');
+        expect(typeof index.Util.getArrayLevel).toBe('function');
+        expect(typeof index.Util.createGUID).toBe('function');
+    });
+});
