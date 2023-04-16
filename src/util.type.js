59958dbe9402554fc6cbfc3d74df57950be6363c
logicfeel <logicfeel@nate.com>
2023-04-17 06:21:58 +0900

    validType 개발 및 테스트

--- src/util.type.js
+++ src/util.type.js
@@ -25,115 +25,121 @@
      * @param {boolean?} isObject Object를 포함 여부
      * @returns {array}  
      */
-        var getAllProperties = function(obj, isObject) {
-            var allProps = [], curr = obj;
-            var is = isObject || false;
-    
-            do{
-                var props = Object.getOwnPropertyNames(curr);
-                props.forEach(function(prop) {
-                    if (allProps.indexOf(prop) === -1 && (is || !Object.prototype.hasOwnProperty(prop)))
-                        allProps.push(prop);
-                });
-            }while(curr = Object.getPrototypeOf(curr))
-            return allProps;
-        }
+    var getAllProperties = function(obj, isObject) {
+        var allProps = [], curr = obj;
+        var is = isObject || false;
+
+        do {
+            var props = Object.getOwnPropertyNames(curr);
+            props.forEach(function(prop) {
+                if (allProps.indexOf(prop) === -1 && (is || !Object.prototype.hasOwnProperty(prop)))
+                    allProps.push(prop);
+            });
+        } while (curr = Object.getPrototypeOf(curr))
+        return allProps;
+    }
 
     var validType = function(type, target, parentName) {
         var parentName = parentName ? parentName : 'this';
-        var obj, typeName;
-        var typeObj = { type:'', default: null };
-        
-        var innerType
+        var typeDef = {type: type, name: '', default: null};
         
         // [] 여부시 OR 조건 재귀검사
         if (Array.isArray(type)) {
-            for(var i = 0; i < type.length; i++) {
-                if (validType(type[i]) === true) return true;
-            }
+            for(var i = 0; i < type.length; i++) if (validType(type[i], target) === true) return true;
             return false;
         }
-        /**
-         * 타입을 걸러내고
-         */
-        if (type === null) 
-            typeObj.type = 'null';
-        else if (typeof type !== 'function' && typeof type !== 'object') 
-            typeObj = {type: typeof type, default: type };
-        else if (typeof type === 'function' && (type === Number || type === String || type === Boolean || type === Object)) 
-            typeObj = {type: typeof type() };
-        else if (type === Array) typeObj = {type: 'array'};
-        else if (typeof type === 'function') typeObj = {type: 'function'};
-        else throw new Error('조건에 맞는 type 이 없습니다.');
-        // else if (typeof type === 'object')
-        // else if (typeof type === 'function' && ())
-
-        // else if (typeof type !== 'object' ) typeName = typeof type;
-
-        // String 과 '' 의 구분
-
-        if (typeof type === 'function') obj = new type();
-        
-        var list = getAllProperties(type)
-
-        for (let i = 0; i < list.length; i++) {
-            const element = list[i];
-            
-        }
-        
+        // 타입 설정
+        if (typeof type !== 'function' && typeof type !== 'object' && typeof type !== 'undefined') {
+            typeDef.name = typeof type;
+            typeDef.default = type;
+            if (typeof target === 'undefined') target = type;   // 기본값 설정 REVIEW: => 필요성 여부
+        } else if (typeof type === 'function' && (type === Number || type === String || type === Boolean || type === Object)) 
+            typeDef.name = typeof type();
+        else if (type === null) typeDef.name = 'null';
+        else if (type === Array) typeDef.name = 'array';
+        else if (type === Function) typeDef.name = 'function';
+        else if (typeof type === 'function') typeDef.name = 'class';
+        else if (typeof type === 'object') typeDef.name = 'object';
+        else throw new Error('타입이 존재하지 않습니다.');
     
+        // 비교
+        if (typeDef.name === 'null' && typeof target !== 'undefined') {
+            return true; 
+        } else if (typeDef.name === 'string' && typeof target === 'string') {
+            return true;
+        } else if (typeDef.name === 'number' && typeof target === 'number') {
+            return true;
+        } else if (typeDef.name === 'boolean' && typeof target === 'boolean') {
+            return true;
+        } else if (typeDef.name === 'function' && typeof target === 'function') {
+            return true;
+        } else if (typeDef.name === 'class') {
+            if (target instanceof typeDef.type) return true;
+            else if (typeof target === 'object') return validType(new typeDef.type, target, parentName);
+            else return false;
+        } else if (typeDef.name === 'object') {
+            var list = getAllProperties(typeDef.type);
+            for (var i = 0; i < list.length; i++) {
+                var key = list[i];
+                var keyValue = typeDef.type[key];
+                // 통과 조건 (예약어, null)
+                if ('_interface' === key || 'isImplementOf' === key
+                    || (type[key] === null && typeof target[key] !== 'undefined')) continue;
+                // 기본값 설정
+                if (typeof keyValue !== 'function' && typeof keyValue !== 'object' 
+                    && typeof keyValue !== 'undefined' && typeof target[key] === 'undefined') target[key] = keyValue;
+
+                if (!(key in target)) {
+                    console.warn(' 대상 없음 ' + parentName + '.' + key + ' : ' + typeof type[key]); 
+                    return false;
+                } else {
+                    if (validType(type[key], target[key], parentName +'.'+ key) === false) return false;
+                }
+            }
+            return true;
+        }
     };
-
-
 
     // OR 조건
     var checkType = function(target, types) {
-        try {
-            var arr = Array.isArray(types) ? types : Array.prototype.slice.call(arguments, 1);
-            for(var i = 0; i < arr.length; i++) {
-                if(validType(target, arr[i]) === true) return true;
+        // try {
+            var arrType = Array.isArray(types) ? types : Array.prototype.slice.call(arguments, 1);
+            for(var i = 0; i < arrType.length; i++) {
+                if(validType(arrType[i], target) === true) return true;
             }
-        } catch (e) {
-            console.error('CheckTypeAll() 에서 오류가 발생했습니다.');
-            console.error(e);
-            return false;
-        }
+        // } catch (e) {
+        //     console.error('CheckTypeAll() 에서 오류가 발생했습니다. ');
+        //     // console.error(e);
+        //     return false;
+        // }
         return false;
     };
 
     // AND 조건
     var checkTypeAll = function(target, types) {
-        try {
-            var arr = Array.isArray(types) ? types : Array.prototype.slice.call(arguments, 1);
-            for(var i = 0; i < arr.length; i++) {
-                if (validType(target, arr[i]) === false) return false;
+        // try {
+            var arrType = Array.isArray(types) ? types : Array.prototype.slice.call(arguments, 1);
+            for(var i = 0; i < arrType.length; i++) {
+                if (validType(arrType[i], target) === false) return false;
             }
-        } catch (e) {
-            console.error('CheckTypeAll() 에서 오류가 발생했습니다.');
-            console.error(e);
-            return false;
-        }
+        // } catch (e) {
+        //     console.error('CheckTypeAll() 에서 오류가 발생했습니다.');
+        //     console.error(e);
+        //     return false;
+        // }
         return true;
     };
 
     //==============================================================
     // 5. 모듈 내보내기 (node | web)
     if (typeof module === 'object' && typeof module.exports === 'object') {     
-        module.exports.inherits = inherits;
-        module.exports.getArrayDepth = getArrayDepth;
-        module.exports.createGUID = createGUID;
-        // module.exports.validSelector = validSelector;   // node 에서는 테스트 불가능!
         module.exports.getAllProperties = getAllProperties;
-        module.exports.equalType = equalType;
-        module.exports.implements = implement;
+        module.exports.checkType = checkType;
+        module.exports.checkTypeAll = checkTypeAll;
     } else {    // COVER:
-        global._L.Common.Util.inherits = inherits;
-        global._L.Common.Util.getArrayDepth = getArrayDepth;
-        global._L.Common.Util.createGUID = createGUID;
-        // global._L.Common.Util.validSelector = validSelector;
         global._L.Common.Util.getAllProperties = getAllProperties;
-        global._L.Common.Util.equalType = equalType;
-        global._L.Common.Util.implements = implement;
+        global._L.Common.Util.checkType = checkType;
+        global._L.Common.Util.checkTypeAll = checkTypeAll;
     }
 
 }(typeof module === 'object' && typeof module.exports === 'object' ? global : window));
---
+++ test/util.type.test.js
@@ -0,0 +1,82 @@
+/**
+ * ES6 + CJS
+ */
+//==============================================================
+// gobal defined
+const {getAllProperties, checkType, checkTypeAll}      = require('../src/util.type');
+
+
+
+
+//==============================================================
+// test
+describe('Util.*', () => {
+    beforeAll(() => {
+        // 경고 모킹 = 숨감
+        global.console.warn = jest.fn();
+    });
+    it('- checkType() : 빈 함수타입 검사 ', () => {
+        var Func1 = function() {};
+
+        // 성공 조건
+        expect(checkType(Func1, Function)).toBe(true);              // 함수
+        expect(checkType(Func1, String, Function)).toBe(true);      // 문자 또는 함수
+        expect(checkType(new Func1(), Func1)).toBe(true);           // new 객체
+        expect(checkType({}, Func1)).toBe(true);                    // 빈 객체
+        // 실패 조건
+        expect(checkType(Func1, Number, '', true)).toBe(false);     // 숫자, 문자, 부울
+        expect(checkType(Func1, String, Number)).toBe(false);       // 문자 또는 숫자
+        expect(checkType(Func1, Func1, Number)).toBe(false);        // 객체 또는 숫자
+        expect(checkType(Func1, Func1)).toBe(false);                // 객체여야함
+    });
+    it('- checkType() : 예외 ', () => {
+        expect(() => checkType({}, undefined)).toThrow(/타입이/);
+    });
+    it('- checkType() : 객체 ', () => {
+        var Func1 = function() { this.aa = String };
+        var Func2 = function() { this.bb = Number };
+        var obj1 = { aa: 'str', bb: 2}
+        var obj2 = { aa: 'str'}
+
+        expect(checkType(obj1, Func1)).toBe(true);
+        expect(checkType(obj1, Func2)).toBe(true);
+        expect(checkType(obj1, Func1, Func2)).toBe(true);
+        expect(checkType(obj2, Func1, Func2)).toBe(true);
+    });
+    it('- checkType() : 상속객체 검사 ', () => {
+    });
+    it('- checkType() : null 검사 ', () => {
+    });
+
+    it('- checkTypeAll() : 인터페이스 ', () => {
+        var Func1 = function() { this.aa = String };
+        var Func2 = function() { this.bb = Number };
+        var obj1 = { aa: 'str', bb: 2}
+        var obj2 = { aa: 'str'}
+        
+        expect(checkTypeAll(obj1, Func1)).toBe(true);
+        expect(checkTypeAll(obj1, Func2)).toBe(true);
+        expect(checkTypeAll(obj1, Func1, Func2)).toBe(true);
+        expect(checkTypeAll(obj2, Func1, Func2)).toBe(false);   // bb를 구현 안됬음
+    });
+    it('- checkTypeAll() : 객체 기본값 ', () => {
+        var Func1 = function() { this.aa = String };
+        var Func2 = function() { this.bb = 0 };
+        var obj1 = { aa: 'str', bb: 2}
+        var obj2 = { aa: 'str'}
+
+        expect(checkTypeAll(obj1, Func1)).toBe(true);
+        expect(checkTypeAll(obj1, Func2)).toBe(true);
+        expect(checkTypeAll(obj1, Func1, Func2)).toBe(true);
+        expect(checkTypeAll(obj2, Func1, Func2)).toBe(true);    // bb 기본값이 설정됨
+        
+        expect(obj2.bb).toBe(0);                                // 초기값 검사
+    });
+    it('- checkTypeAll() : 원시 가본값 ', () => {
+        var str = 'STR';
+        var obj;
+        
+        expect(checkTypeAll(obj, str)).toBe(true);
+        expect(obj).not.toBe('STR');                            // 값이 전달되어 값이 설정 안됨
+    });
+});
