2a04e1396a33c4e4149c7fff157e28673a3b1b7b
logicfeel <logicfeel@gmail.com>
2023-11-09 16:24:01 +0900

    샘플 추가

--- temp/오토조립샘플.js
+++ temp/오토조립샘플.js
@@ -11,6 +11,14 @@ function Auto(){
     var a3 = new Auto3();
     
     a1.title = '제막'
-    a1.model = 
+    a1.model = 100;
     this.mod(a1);
+
+    a2.title = '목록';
+    a2.prop.view = a1.view;
+    
+    a3.caption = '설명';
+    a3.datasource = source;
+    
+
 }
