cd830b74da2c3ee1119185718771c389362ef787
logic <logicfeel@gmail.com>
2023-11-09 14:30:21 +0900

    조립샘플

---
+++ temp/오토조립샘플.js
@@ -0,0 +1,16 @@
+
+
+var Auto1;
+var Auto2;
+var Auto3;
+
+function Auto(){
+    
+    var a1 = new Auto1();
+    var a2 = new Auto2();
+    var a3 = new Auto3();
+    
+    a1.title = '제막'
+    a1.model = 
+    this.mod(a1);
+}
