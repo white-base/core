62eb1d9a2bbf3e11e070cc6dfc9e1e4a98ef33df
logicfeel <logicfeel@gmail.com>
2023-12-03 16:36:38 +0900

    타입샘플2

---
+++ temp/타입샘플2.js
@@ -0,0 +1,20 @@
+/**
+ * 인터페이스 샘플
+ */
+
+// 객체 형식으로 정의 1
+var iObj = {
+    name: String,
+    age: Number
+}
+// 객체 형식으로 정의 2
+var iObj = {
+    name: '',
+    age: 0
+}
+// 클래스 형식으로 정의
+class Obj {
+    name = String;
+    age = 0
+}
+
