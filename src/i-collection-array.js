a4fcd9ba3d217db7ab44067841673c784d5e9240
logic <128760842+white-base@users.noreply.github.com>
2025-04-01 03:53:08 +0900

    i-collection-array.js 업데이트

--- src/i-collection-array.js
+++ src/i-collection-array.js
@@ -1,19 +1,13 @@
 /**** i-collection-array.js | IArrayCollection ****/
 //==============================================================
-// 1. import module
+// import module
 import Message from './message.js';    
 import ExtendError from './extend-error.js';   
 import Util from './util.js'; 
 import ICollection from './i-collection.js';
 
 //==============================================================
-// 2. module dependency check
-// if (!ExtendError) throw new Error(Message.get('ES011', ['ExtendError', 'extend-error']));
-// if (!Util) throw new Error(Message.get('ES011', ['Util', 'util']));
-// if (!ICollection) throw new Error(Message.get('ES011', ['ICollection', 'i-collection']));
-
-//==============================================================
-// 3. module implementation   
+// module implementation   
 var IArrayCollection  = (function (_super) {
     /**
      * Array collection interface.  
@@ -42,6 +36,6 @@ var IArrayCollection  = (function (_super) {
 }(ICollection));
 
 //==============================================================
-// 4. module export
+// module export
 export default IArrayCollection;
 export { IArrayCollection };
