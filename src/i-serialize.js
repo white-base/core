879791d5615ca8902dcc4c1cc47e910c38128158
logic <128760842+white-base@users.noreply.github.com>
2025-04-01 04:05:44 +0900

    i-serialize.js 업데이트

--- src/i-serialize.js
+++ src/i-serialize.js
@@ -1,15 +1,11 @@
 /**** i-serialize.js | ISerialize ****/
 //==============================================================
-// 1. import module
+// import module
 import Message from './message.js';    
 import ExtendError from './extend-error.js';  
 
 //==============================================================
-// 2. module dependency check
-// if (!ExtendError) throw new Error(Message.get('ES011', ['ExtendError', 'extend-error']));
-
-//==============================================================
-// 3. module implementation   
+// module implementation   
 var ISerialize  = (function () {
     /**
      * Interface for serialization and deserialization.  
@@ -46,6 +42,6 @@ var ISerialize  = (function () {
 }());
 
 //==============================================================
-// 4. module export
+// module export
 export default ISerialize;
 export { ISerialize };
